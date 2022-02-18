a98ca35b3ab2cca42df755eb0a11a2312667875c
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2022-02-18 10:14:51 +0700

    Export value_notifier_list_extensions

--- lib/we_it.dart
+++ lib/we_it.dart
@@ -1,5 +1,6 @@
 library we_it;
 
+export 'src/extensions/value_notifier_list_extensions.dart';
 export 'src/w_infinite_list/w_infinite_list_builder_sliver.dart';
 export 'src/w_infinite_list/w_infinite_list_state.dart';
 export 'src/w_listener.dart';
